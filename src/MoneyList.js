4a1b4b7edbf926d1f5688186a7016ec01002ae9f
sarah1446 <lemonview2@gmail.com>
2020-06-17 16:58:24 +0900

    수정사항 작업중

--- src/MoneyList.js
+++ src/MoneyList.js
@@ -4,19 +4,23 @@ class MoneyList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: ''
+      content: '',
+      time: '',
+      editingMode: false
     }
   }
 
   deleteList = () => {
-    // this.props.deleteList(this.props.moneyList);
     this.props.deleteList(this.props.list, this.props.list.price);
   }
 
-  // totalPrice = () => {
-  //   this.props.
-  // }
-
+  editing = (e) => {
+    this.setState({
+      editingMode: true,
+      content: e.target.value,
+      price: e.target.value
+    })
+  }
 
   render() {
     const { content, price, time } = this.props.list;
@@ -25,7 +29,15 @@ class MoneyList extends React.Component {
         <span onClick={this.deleteList}>x</span>
         <ul className="list-wrap">
           <li className="time">{time}</li>
-          <li className="cont">{content}</li>
+          <li className="cont" onDoubleClick={this.editing}>
+            {this.state.editingMode === false ?
+              //내용
+              content
+              :
+              //수정모드 
+              <input type="text" defaultValue={content} />
+            }
+          </li>
           <li className="price">{price}</li>
         </ul>
       </div>
